867fcf35d7087c2600c58c57134e574eeb2ed7a5
Ilya Kostrikov <ikostrikov@gmail.com>
2021-08-09 01:35:20 +0000

    Refactor absorbing states.

--- jaxrl/wrappers/absorbing_states.py
+++ jaxrl/wrappers/absorbing_states.py
@@ -3,31 +3,32 @@ import numpy as np
 from gym import Wrapper
 
 
+def make_non_absorbing(observation):
+    return np.concatenate([observation, [0.0]], -1)
+
+
 class AbsorbingStatesWrapper(Wrapper):
     def __init__(self, env):
         super().__init__(env)
         low = env.observation_space.low
         high = env.observation_space.high
+        self._absorbing_state = np.concatenate([np.zeros_like(low), [1.0]], 0)
         low = np.concatenate([low, [0]], 0)
         high = np.concatenate([high, [1]], 0)
-        self._absorbing_state = np.ones_like(low)
 
         self.observation_space = gym.spaces.Box(
             low=low, high=high, dtype=env.observation_space.dtype)
-
-    def _make_absorbing(self, observation):
-        return np.concatenate([observation, [0.0]], 0)
 
     def reset(self, **kwargs):
         self._done = False
         self._absorbing = False
         self._info = {}
-        return self._make_absorbing(self.env.reset(**kwargs))
+        return make_non_absorbing(self.env.reset(**kwargs))
 
     def step(self, action):
         if not self._done:
             observation, reward, done, info = self.env.step(action)
-            observation = self._make_absorbing(observation)
+            observation = make_non_absorbing(observation)
             self._done = done
             self._info = info
             truncated_done = 'TimeLimit.truncated' in info
