374e303000d47a41948af0eeb453d310bd2e3605
Ilya Kostrikov <ikostrikov@gmail.com>
2021-10-19 11:33:31 -0700

    Update tfp version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
     'gym[mujoco] >= 0.18.0', 'gdown >= 3.12.2', 'tqdm >= 4.60.0',
     'flax >= 0.3.5', 'jax >= 0.2.21', 'ml_collections >= 0.1.0',
     'optax >= 0.0.6', 'tensorboardX == 2.1',
-    'tensorflow-probability >= 0.13.0', 'imageio >= 2.9.0',
+    'tfp-nightly', 'imageio >= 2.9.0',
     'imageio-ffmpeg >= 0.4.3',
     'd4rl @ git+https://github.com/rail-berkeley/d4rl@master#egg=d4rl'
 ]
